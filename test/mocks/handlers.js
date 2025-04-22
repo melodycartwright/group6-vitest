d9fc0dc6162a4b1eacc185aecb4043e0dc8918c4
Melody Cartwright <melody94c@gmail.com>
2025-04-22 15:08:41 +0200

    bug fix

--- test/mocks/handlers.js
+++ test/mocks/handlers.js
@@ -1,20 +1,22 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse } from "msw";
 
 const API_BASE = "https://tokenservice-jwt-2025.fly.dev";
-//Fake api endpoints for handelrs
+
 export const handlers = [
   http.post(`${API_BASE}/token-service/v1/request-token`, async () => {
     return new HttpResponse("mocked-token", { status: 200 });
   }),
-  http.get(`${API_BASE}/movies`, ({ request }) => {
-    const authHeader = request.headers.get('Authorization');
 
-    if (authHeader === 'Bearer mocked-token') {
-        return HttpResponse.json([
-            {title: 'Group 6 movie title'}
-            {title: 'And another movie!'}
-        ]);
+  http.get(`${API_BASE}/movies`, ({ request }) => {
+    const authHeader = request.headers.get("Authorization");
+
+    if (authHeader === "Bearer mocked-token") {
+      return HttpResponse.json([
+        { title: "The Matrix" },
+        { title: "Inception" },
+      ]);
     }
-    return HttpResponse.json({ message: 'Forbidden'}, { status: 403});
+
+    return HttpResponse.json({ message: "Forbidden" }, { status: 403 });
   }),
 ];
