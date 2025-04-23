d3e11bb74169266bcd0c4ef12b3a239d3617e341
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-04-23 14:08:14 +0200

    create comp, added states

---
+++ src/components/movielist/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const [token, setToken] = useState("");
+  const [error, setError] = useState("");
+
+  return <></>;
+};
+export default MovieList;
